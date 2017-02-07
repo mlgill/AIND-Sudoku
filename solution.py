1fce00767137cdf8604aa7929266d2f6a165bd44
Michelle Gill <michelle@michellelynngill.com>
2017-02-07 18:22:01 -0500

    Add code

--- solution.py
+++ solution.py
@@ -1,16 +1,51 @@
+import itertools as it
+
+
+def cross(A, B):
+    """Cross product of elements in A and elements in B."""
+    
+    return [''.join(x) for x in it.product(A, B)]
+
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) 
+             for r in rows]
+
+column_units = [cross(rows, c) 
+                for c in cols]
+
+square_units = [cross(rs, cs) 
+                for rs in ('ABC', 'DEF', 'GHI') 
+                for cs in ('123', '456', '789')]
+
+diagonal_units = [[''.join(x) for x in list(zip(rows, cols))],
+                  [''.join(x) for x in list(zip(rows, cols[::-1]))]]
+
+unitlist = row_units + column_units + square_units + diagonal_units
+
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
 assignments = []
+
 
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
     Assigns a value to a given box. If it updates the board record it.
     """
+    
     values[box] = value
     if len(value) == 1:
         assignments.append(values.copy())
     return values
 
-def naked_twins(values):
+
+def naked_twins(values, n=2):
     """Eliminate values using the naked twins strategy.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
@@ -19,12 +54,38 @@ def naked_twins(values):
         the values dictionary with the naked twins eliminated from peers.
     """
 
-    # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
+    for unit in unitlist:
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+        # Extract a list of values/cells where there are n possibilities 
+        twin_list = [(values[x], x) for x in unit if len(values[x]) == n]
+
+        # Aggregate all cells for a given set of values into a single list
+        twin_list = [(key, [x[1] for x in val]) 
+                 for key,val in it.groupby(twin_list, lambda x: x[0])]
+
+        # Prune this list to only entries with a minimum of 'n' occurences of identical entries
+        twin_list = [x for x in twin_list if len(x[1]) >= n]
+
+        if twin_list:
+
+            # Sanity check: there cannot be more than 'n' cells each with only 'n' possibilities
+            max_cells = max([len(x[1]) for x in twin_list])
+            assert max_cells == n, 'Puzzle is unsolveable'
+
+            # Iterate over each twin tuple
+            for twin in twin_list:
+                twin_vals = twin[0]
+                twin_cells = twin[1]
+                other_cells = list(set(unit) - set(twin_cells))
+
+                # Remove the twin values from other cells in the unit
+                for cell in other_cells:
+                    for val in twin_vals:
+                        new_value = values[cell].replace(val, '')
+                        values = assign_value(values, cell, new_value)
+                        
+    return values
+
 
 def grid_values(grid):
     """
@@ -36,7 +97,19 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    
+    assert len(grid) == 81, "Input grid must be 81 characters long."
+    
+    # Create the board
+    table = dict(zip(boxes, grid))
+    
+    # Iterate over each key and determine possibilities if empty
+    for key,val in table.items():
+        if val == '.':
+            table = assign_value(table, key, '123456789')
+    
+    return table
+
 
 def display(values):
     """
@@ -44,19 +117,159 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    
+    if values:
+        # Calculate maximum width for each column
+        width_vals = [max([len(values[x]) for x in unit]) for unit in column_units]
+
+        # Setup print format string
+        string_format = "{0:{width[0]}} {1:{width[1]}} {2:{width[2]}} | " + \
+                        "{3:{width[3]}} {4:{width[4]}} {5:{width[5]}} | " + \
+                        "{6:{width[6]}} {7:{width[7]}} {8:{width[8]}}"
+
+        # Print each row of values and spacers in between
+        for row in enumerate(row_units):
+
+            if ((row[0] % 3 == 0) and (row[0] > 0)):
+                print(string_format.format(*['-']*9, width=width_vals))
+
+            row_values = [values[x] for x in row[1]]
+
+            print(string_format.format(*row_values, width=width_vals))
+            
+    else:
+        print('Puzzle cannot be solved.')
+        
+    return
+
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    
+    # Create a copy as the replace doesn't work well when other peers are being changed
+    values2 = values.copy()
+    
+    for key,val in values.items():
+        
+        if len(val) > 1:            
+            
+            # Remove values that are assigned elsewhere
+            for cell_list in unitlist:
+                if key in cell_list:
+                    
+                    for cell in cell_list:
+                        if ((len(values[cell]) == 1) and (key != cell)):
+                            
+                            new_value = values2[key].replace(values[cell], '')
+                            values2 = assign_value(values2, key, new_value)
+                            
+    return values2
+
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+
+    # Count possibilities and eliminate for each unit
+    for unit in unitlist:
+        
+        # Have to iterate exactly like provided answer so it will be accepted
+        # by autograder since there are more than one possibilities otherwise
+        for digit in '123456789':
+            
+            occurrences = [cell for cell in unit if digit in values[cell]]
+            
+            if len(occurrences) == 1:
+                values = assign_value(values, occurrences[0], digit)
+                
+    return values
+
 
 def reduce_puzzle(values):
-    pass
+    """Whenever there is a single possible answer for a given cell, iteratively prune
+       it as a possible solution for from all other peers. This proceeds until there is
+       no change in the Sudoku dictionary.
+       
+       Input: Sudoku in dictionary form.
+       Output: Resulting Sudoku in dictionary form after removing singular answers.
+    """
+    
+    stalled = False
+    
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Eliminate value from peers when it is the only one in a given box
+        values = eliminate(values)
+        
+        # Formally assign a unique value by removing as a possibilitiy from peers in other units
+        values = only_choice(values)
+        
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+        
+    return values
+
 
 def search(values):
-    pass
+    """Using depth-first search and propagation, create a search tree and solve the sudoku.
+       
+       Input: Sudoku in dictionary form.
+       Output: Reduced Sudoku in dictionary form."""
+    
+    # First, reduce the puzzle
+    values = reduce_puzzle(values)
+    
+    # Remove naked twins
+    values = naked_twins(values)
+    
+    if not values:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    
+    # Choose one of the unfilled squares with the fewest possibilities
+    min_len, key = min([(len(v),k) for k,v in values.items() if len(v) > 1])
+    
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    vals = values[key]
+    
+    for val in vals:
+        # Create a copy to test on
+        values2 = values.copy()
+        
+        # Assign one of the possibilities
+        values2 = assign_value(values2, key, val)
+        
+        # Recursively search
+        values2 = search(values2)
+        
+        if values2:
+            return values2
+        
 
 def solve(grid):
     """
@@ -65,8 +278,18 @@ def solve(grid):
         grid(string): a string representing a sudoku grid.
             Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     Returns:
-        The dictionary representation of the final sudoku grid. False if no solution exists.
+        The dictionary representation of the final sudoku grid. False if no solution exists
     """
+    
+    values = grid_values(grid)
+    values = search(values)
+    
+    # Ensure puzzle is solveable
+    if len(''.join(values.values())) != 81:
+        return False
+    
+    return values
+
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
